6d568e1bcb933aa478fce251db8119245b182f2a
guirecordon <guilherme.recordon@gmail.com>
2024-07-02 21:14:25 -0300

    first commit

---
+++ src/main/java/dev/wafflestack/cicdpipelines/CicdpipelinesApplication.java
@@ -0,0 +1,13 @@
+package dev.wafflestack.cicdpipelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdpipelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdpipelinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wafflestack/cicdpipelines/CicdpipelinesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wafflestack.cicdpipelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdpipelinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
